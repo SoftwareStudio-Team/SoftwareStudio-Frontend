c9e28e58203e8a67deb9be35ebfbdbbe4852865b
goobest <goobest3@gmail.com>
2022-04-24 03:56:38 +0700

    deletepost

--- src/comment/CommentCard.jsx
+++ src/comment/CommentCard.jsx
@@ -4,83 +4,97 @@ import { useEffect, useState } from 'react';
 import CommentsApi from '../api/comments';
 import { NavLink, useParams } from 'react-router-dom';
 import { useUser } from '../state/user/hook';
+import { toast } from 'react-toastify';
 
-const CommentCard = ({ comment,index }) => {
-    const {user} =useUser();
-    
-   const [commentId,setCommentId] = useState("")
-   const [usercomId,setUsercomId] = useState("")
-   const [likecomment,setlikecomment] =useState(0)
-    const[islike,setIslike]=useState(false)
-   useEffect(()=>{
-    setCommentId(comment.id.toString())
+const CommentCard = ({ comment, index }) => {
+  const { user } = useUser();
+  console.log(comment);
+  const [commentId, setCommentId] = useState('');
+  const [commentuserid, setCommentuserId] = useState('');
+  const [usercomId, setUsercomId] = useState('');
+  const [likecomment, setlikecomment] = useState(0);
+  const [islike, setIslike] = useState(false);
+  useEffect(() => {
+    setCommentId(comment.id.toString());
+    setCommentuserId(comment.owner.id.toString());
     pushcomment();
-   },[]);
-   const pushcomment = async () => {
-    
-    
+  }, []);
+  const pushcomment = async () => {
     try {
-       
-        const {data} =await CommentsApi.getById({id:commentId} );
-        setlikecomment(data[index].likes.length)
-        console.log(data)
-        // console.log(data)
-        data[index].likes.forEach(userlike => {
-            if(user.id === userlike.id){
-                return setIslike(true)
-            }
-        });
-       
+      const { data } = await CommentsApi.getById({ id: commentId });
+      setlikecomment(data[index].likes.length);
+      console.log(data);
+      // console.log(data)
+      data[index].likes.forEach((userlike) => {
+        if (user.id === userlike.id) {
+          return setIslike(true);
+        }
+      });
+
       // setComment(data.comments);
       // console.log(comment);
     } catch (err) {
-    //   error(err.response.data.message);
+      //   error(err.response.data.message);
     }
-    
   };
 
   const handlelike = async () => {
-    
-    if (islike === false){
-    try {
-       
-        await CommentsApi.like({id:commentId} );
-     
-      console.log("like สำเร็จ")
-      setlikecomment(likecomment+1)
-      setIslike(true)
+    if (islike === false) {
+      try {
+        await CommentsApi.like({ id: commentId });
 
-      // setComment(data.comments);
-      // console.log(comment);
-    } catch (err) {
-    //   error(err.response.data.message);
+        console.log('like สำเร็จ');
+        setlikecomment(likecomment + 1);
+        setIslike(true);
+
+        // setComment(data.comments);
+        // console.log(comment);
+      } catch (err) {
+        //   error(err.response.data.message);
+      }
     }
-    
-  }};
-  
+  };
+
   const handleunlike = async () => {
-    
-    if(islike === true){
-    try {
-        
-       
-        await CommentsApi.unlike({id:commentId} );
-        setlikecomment(likecomment-1)
-        setIslike(false)
-      console.log("unlike สำเร็จ")
-      // setComment(data.comments);
-      // console.log(comment);
-    } catch (err) {
-    //   error(err.response.data.message);
+    if (islike === true) {
+      try {
+        await CommentsApi.unlike({ id: commentId });
+        setlikecomment(likecomment - 1);
+        setIslike(false);
+        console.log('unlike สำเร็จ');
+        // setComment(data.comments);
+        // console.log(comment);
+      } catch (err) {
+        //   error(err.response.data.message);
+      }
     }
-    
-  }};
-
-  
+  };
+  const deletepost = async () => {
+    if (commentuserid === user.id || user.role === 'admin') {
+      try {
+        await CommentsApi.delete({ id: commentId });
+        window.location.reload();
+      } catch (err) {
+        
+      }
+    }else{
+        toast.error("ไม่สามารถลบได้")
+    }
+  };
 
   return (
     <div className="flex flex-col w-full h-full bg-white max-w-full rounded-2xl px-10 py-8 mt-5 shadow-lg hover:shadow-2xl transition duration-500">
-      <p className="mt-4 text-md text-gray-600">{comment.commentMessage}</p>
+      <div className="flex flex-row justify-between">
+        <p className="mt-4 text-md text-gray-600">{comment.commentMessage}</p>
+        <div className="items-end">
+          <button
+            className="text-white px-4 w-auto h-10 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
+            onClick={deletepost}
+          >
+            delete
+          </button>
+        </div>
+      </div>
       <div className="flex justify-between items-center">
         <div className="mt-4 flex items-center space-x-4 py-6">
           <div className="text-sm font-semibold">
@@ -92,7 +106,10 @@ const CommentCard = ({ comment,index }) => {
           </div>
         </div>
         <div className="">
-          <button className="text-white px-4 w-auto h-10 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none" onClick={islike ? handleunlike : handlelike} >
+          <button
+            className="text-white px-4 w-auto h-10 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
+            onClick={islike ? handleunlike : handlelike}
+          >
             {likecomment}
             <svg
               viewBox="0 0 20 20"
