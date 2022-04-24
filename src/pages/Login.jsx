5d4d41a9146ffc7b2b4400304f364f67cdc87dd6
Nattapon Sukthaworn <nattapon33998@gmail.com>
2022-04-24 18:56:23 +0700

    chore: logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,11 +30,12 @@ const LoginPage = () => {
         <div>
           <img
             className="mx-auto h-12 w-auto"
+            // src={'./'}
             src="https://static.thairath.co.th/media/4DQpjUtzLUwmJZZSCHsWfqESsCfx73Hpp9TiDFICc2wj.jpg"
           />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <p className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
-          </h2>
+          </p>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or
             <a
